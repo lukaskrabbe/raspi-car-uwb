095bbb4c91a142c65702a6e5908bee3236e2889a
Lukas Krabbe <mail@l-krabbe.de>
2020-11-09 16:11:11 +0100

    First Drive

--- car_controller.py
+++ car_controller.py
@@ -7,14 +7,14 @@ import time
 if __name__ == '__main__':
     base.set_speed(0.0, 0.1)
 
-    time.wait(1)
+    time.sleep(1)
 
     base.set_speed(0.0, 0.2)
 
-    time.wait(2)
+    time.sleep(2)
 
     base.set_speed(0.0, 0.1)
 
-    time.wait(1)
+    time.sleep(1)
 
     base.set_speed(0.0, 0.0)
