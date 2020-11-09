f333cdedf7097264f2a851e25418c2e9fbec8493
Lukas Krabbe <mail@l-krabbe.de>
2020-11-09 16:02:07 +0100

    First Drive

--- car_controller.py
+++ car_controller.py
@@ -1,11 +1,20 @@
-
-
-
-
-
+from uni_scripts import base
+import time
 
 
 
 
 if __name__ == '__main__':
-    print("asd")
+    base.set_speed(0.0, 0.1)
+
+    time.wait(1)
+
+    base.set_speed(0.0, 0.2)
+
+    time.wait(2)
+
+    base.set_speed(0.0, 0.1)
+
+    time.wait(1)
+
+    base.set_speed(0.0, 0.0)
