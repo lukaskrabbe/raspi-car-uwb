70b202464620d882330dccedc03c47076501a278
Lukas Krabbe <mail@l-krabbe.de>
2020-11-09 16:59:52 +0100

    added left

--- car_controller.py
+++ car_controller.py
@@ -38,7 +38,7 @@ def left(angle=90):
     if angle > 180:
         raise ValueError('The angle has to be smaller than 180')
 
-    hold_time = (angle/20)
+    hold_time = (angle/20.2)
 
     base.set_speed(0.0, 0.1)
 
